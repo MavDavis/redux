0f6ed59e46ff6f89cd0b3a79d06cdcc4804f550a
Mavdavis <ekwesimarvelous@gmail.com>
2022-12-15 14:51:42 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Interest from "./components/Interest";
+import Info from "./components/Info";
+import Footer from "./components/Footer";
+function App({title,paragraph}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <Interest title={'About'} paragraph={'Meticulous web developer with over 3 years of frontend experience and passion for responsive and aesthetic design. Worked on over 12  large scale projects both independently and with teams of  5 - 30.'}/>
+
+      <Interest title={'Interest'} paragraph={'Blockchain learning, Seo, data structures, Artificial Intelligence and more as I grow.'}/>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Interest.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Interest(props) {
+  return (
+    <div className="props">
+      <h3>{props.title}</h3>
+      <p>{props.paragraph}</p>
+    </div>
+  );
+}
